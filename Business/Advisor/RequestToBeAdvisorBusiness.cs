ab289b775ba742c4ace650a50fe2b6b681b7d4fb
arinyguedes <arinyguedes@gmail.com>
2018-07-26 19:02:09 -0300

    User methods

--- Business/Advisor/RequestToBeAdvisorBusiness.cs
+++ Business/Advisor/RequestToBeAdvisorBusiness.cs
@@ -14,5 +14,66 @@ namespace Auctus.Business.Advisor
     public class RequestToBeAdvisorBusiness : BaseBusiness<RequestToBeAdvisor, RequestToBeAdvisorData>
     {
         public RequestToBeAdvisorBusiness(ILoggerFactory loggerFactory, Cache cache) : base(loggerFactory, cache) { }
-    }
+
+        public RequestToBeAdvisor GetByUser(int userId)
+        {
+            return Data.GetByUser(userId);
+        }
+
+        public async Task<RequestToBeAdvisor> Create(string email, string name, string description, string previousExperience)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name must be filled.");
+            if (name.Length > 100)
+                throw new ArgumentException("Name cannot have more than 100 characters.");
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Description must be filled.");
+            if (description.Length > 4000)
+                throw new ArgumentException("Description cannot have more than 4000 characters.");
+            if (string.IsNullOrWhiteSpace(previousExperience))
+                throw new ArgumentException("Previous experience must be filled.");
+            if (previousExperience.Length > 4000)
+                throw new ArgumentException("Previous experience cannot have more than 4000 characters.");
+
+            var user = UserBusiness.GetValidUser(email);
+            var request = GetByUser(user.Id);
+            if (request?.Approved ?? false)
+                throw new ArgumentException("User was already approved as advisor.");
+
+            var newRequest = new RequestToBeAdvisor()
+            {
+                CreationDate = DateTime.UtcNow,
+                Name = name,
+                Description = description,
+                PreviousExperience = previousExperience,
+                UserId = user.Id
+            };
+            Data.Insert(newRequest);
+            await SendRequestToBeAdvisorEmail(user, newRequest, request);
+            return newRequest;
+        }
+        private async Task SendRequestToBeAdvisorEmail(User user, RequestToBeAdvisor newRequestToBeAdvisor, RequestToBeAdvisor oldRequestToBeAdvisor)
+        {
+            await Email.SendAsync(Config.EMAIL_FOR_CRITICAL_ERROR,
+                string.Format("[{0}] Request to be adivosr - Auctus Beta", oldRequestToBeAdvisor == null ? "NEW" : "UPDATE"),
+                string.Format(@"Email: {0} 
+<br/>
+<br/>
+<b>Old Name</b>: {1}
+<br/>
+<b>New Name</b>: {2}
+<br/>
+<br/>
+<b>Old Description</b>: {3}
+<br/>
+<b>New Description</b>: {4}
+<br/>
+<br/>
+<b>Old Previous Experience</b>: {5}
+<br/>
+<b>New Previous Experience</b>: {6}", user.Email, 
+oldRequestToBeAdvisor?.Name ?? "N/A", newRequestToBeAdvisor.Name,
+oldRequestToBeAdvisor?.Description ?? "N/A", newRequestToBeAdvisor.Description,
+oldRequestToBeAdvisor?.PreviousExperience ?? "N/A", newRequestToBeAdvisor.PreviousExperience));
+        }    }
 }
--- DataAccess/Account/UserData.cs
+++ DataAccess/Account/UserData.cs
@@ -1,5 +1,6 @@
 ﻿using Auctus.DataAccess.Core;
 using Auctus.DomainObjects.Account;
+using Auctus.DomainObjects.Advisor;
 using Dapper;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,92 @@ namespace Auctus.DataAccess.Account
     public class UserData : BaseSQL<User>
     {
         public override string TableName => "User";
+
+        private const string SQL_FOR_LOGIN = @"SELECT u.*, a.*, r.*, w.* 
+                                                FROM 
+                                                [User] u
+                                                LEFT JOIN [Advisor] a ON a.Id = u.Id
+                                                LEFT JOIN [RequestToBeAdvisor] r ON r.UserId = u.Id AND r.CreationDate = (SELECT MAX(r2.CreationDate) FROM [RequestToBeAdvisor] r2 WHERE r2.UserId = u.Id)
+                                                LEFT JOIN [Wallet] w ON w.UserId = u.Id AND w.CreationDate = (SELECT MAX(w2.CreationDate) FROM [Wallet] w2 WHERE w2.UserId = u.Id)
+                                                WHERE 
+                                                u.Email = @Email";
+
+        private const string SQL_FOR_CONFIRMATION = @"SELECT u.*, a.*, r.*, w.* 
+                                                FROM 
+                                                [User] u
+                                                LEFT JOIN [RequestToBeAdvisor] r ON r.UserId = u.Id AND r.CreationDate = (SELECT MAX(r2.CreationDate) FROM [RequestToBeAdvisor] r2 WHERE r2.UserId = u.Id)
+                                                WHERE 
+                                                u.ConfirmationCode = @Code";
+
+        private const string SQL_BY_EMAIL = @"SELECT u.*, a.*
+                                                FROM 
+                                                [User] u
+                                                LEFT JOIN [Advisor] a ON a.Id = u.Id
+                                                WHERE 
+                                                u.Email = @Email";
+
+        public User GetForLogin(string email)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Email", email.ToLower().Trim(), DbType.AnsiString);
+            return Query<User, Auctus.DomainObjects.Advisor.Advisor, RequestToBeAdvisor, Wallet, User>(SQL_FOR_LOGIN,
+                        (user, advisor, request, wallet) =>
+                        {
+                            if (advisor != null)
+                            {
+                                advisor.Id = user.Id;
+                                advisor.Email = user.Email;
+                                advisor.Password = user.Password;
+                                advisor.CreationDate = user.CreationDate;
+                                advisor.ConfirmationCode = user.ConfirmationCode;
+                                advisor.ConfirmationDate = user.ConfirmationDate;
+                                advisor.IsAdvisor = true;
+                                advisor.Wallet = wallet;
+                                advisor.RequestToBeAdvisor = request;
+                                return advisor;
+                            }
+                            else
+                            {
+                                user.Wallet = wallet;
+                                user.RequestToBeAdvisor = request;
+                                return user;
+                            }
+                        }, "Id,Id,Id", parameters).SingleOrDefault();
+        }
+
+        public User GetByConfirmationCode(string code)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Code", code, DbType.AnsiString);
+            return Query<User, RequestToBeAdvisor, User>(SQL_FOR_LOGIN,
+                        (user, request) =>
+                        {
+                            user.RequestToBeAdvisor = request;
+                            return user;
+                        }, "Id", parameters).SingleOrDefault();
+        }
+
+        public User GetByEmail(string email)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Email", email.ToLower().Trim(), DbType.AnsiString);
+            return Query<User, Auctus.DomainObjects.Advisor.Advisor, User>(SQL_BY_EMAIL,
+                        (user, advisor) =>
+                        {
+                            if (advisor != null)
+                            {
+                                advisor.Id = user.Id;
+                                advisor.Email = user.Email;
+                                advisor.Password = user.Password;
+                                advisor.CreationDate = user.CreationDate;
+                                advisor.ConfirmationCode = user.ConfirmationCode;
+                                advisor.ConfirmationDate = user.ConfirmationDate;
+                                advisor.IsAdvisor = true;
+                                return advisor;
+                            }
+                            else
+                                return user;
+                        }, "Id", parameters).SingleOrDefault();
+        }
     }
 }
