f1d14ba0d05630ffcd586e0068d82693b19359ff
Thiago Araujo <thiagomvaraujo@gmail.com>
2018-07-26 18:53:48 -0300

    First adjusments

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 import { AppComponent } from './app.component';
+
+import { Web3Service } from './services/web3.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { HttpService } from './services/http.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SimpleNotificationsModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    LocalStorageService,
+    Web3Service ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
