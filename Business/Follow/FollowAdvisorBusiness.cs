0f0e418a4aa9ff725627e694c0c550404354837d
danielduartef <danielduartefigueiredo@gmail.com>
2018-08-07 14:29:25 -0300

    implementation of follow/unfollow backend logic

--- Business/Follow/FollowAdvisorBusiness.cs
+++ Business/Follow/FollowAdvisorBusiness.cs
@@ -1,4 +1,5 @@
-﻿using Auctus.DataAccess.Follow;
+﻿using Auctus.DataAccess.Core;
+using Auctus.DataAccess.Follow;
 using Auctus.DomainObjects.Follow;
 using Auctus.Util;
 using Microsoft.Extensions.Logging;
@@ -11,5 +12,19 @@ namespace Auctus.Business.Follow
     public class FollowAdvisorBusiness : BaseBusiness<FollowAdvisor, FollowAdvisorData>
     {
         public FollowAdvisorBusiness(ILoggerFactory loggerFactory, Cache cache, string email, string ip) : base(loggerFactory, cache, email, ip) { }
+
+        public void Create(int userId, int advisorId, FollowActionType actionType)
+        {
+            using (var transaction = new TransactionalDapperCommand())
+            {
+                var follow = FollowBusiness.Create(userId, actionType);
+
+                var followAdvisor = new FollowAdvisor();
+                followAdvisor.Id = follow.Id;
+                followAdvisor.AdvisorId = advisorId;
+
+                Data.Insert(followAdvisor);
+            }
+        }
     }
 }
