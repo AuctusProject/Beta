5e6d3156b2c13a67cdc23456433f8417bf3de28a
Daniel Silveira <dansilveira@gmail.com>
2018-08-14 12:36:57 -0300

    list-assets

--- Web/src/app/components/asset/asset-card/asset-card.component.ts
+++ Web/src/app/components/asset/asset-card/asset-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AssetResponse } from '../../../model/asset/assetResponse';
+import { AssetService } from '../../../services/asset.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'asset-card',
@@ -8,9 +10,18 @@ import { AssetResponse } from '../../../model/asset/assetResponse';
 })
 export class AssetCardComponent implements OnInit {
   @Input() asset: AssetResponse;
-  constructor() { }
+  constructor(private assetService:AssetService) { }
 
   ngOnInit() {
   }
+  onFollowClick(){
+    this.assetService.followAsset(this.asset.assetId).subscribe(result =>this.asset.following = true);
+  }
+  onUnfollowClick(){
+    this.assetService.unfollowAsset(this.asset.assetId).subscribe(result =>this.asset.following = false);
+  }
 
+  getAdvisorImgUrl(){
+    return environment.assetImgUrl.replace("{id}", this.asset.assetId.toString());
+  }
 }
