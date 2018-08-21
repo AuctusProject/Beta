4a1acb23f4a85fd79395a2efb9de7db9ac06613e
thiagomva <thiagomvaraujo@gmail.com>
2018-08-21 18:20:47 -0300

    Merge adjusment

--- DataAccessMock/Account/UserData.cs
+++ DataAccessMock/Account/UserData.cs
@@ -229,7 +229,7 @@ namespace Auctus.DataAccessMock.Account
 
         public List<User> ListUsersFollowingAdvisorOrAsset(int advisorId, int assetId)
         {
-            throw new NotImplementedException();
+            return new List<User>();
         }
 
         public override void Insert(User obj)
