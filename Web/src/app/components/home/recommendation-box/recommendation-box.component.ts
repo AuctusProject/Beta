b261710b75e670212af458b5b5e11ef649dc8be2
danielduartef <danielduartefigueiredo@gmail.com>
2018-08-29 16:26:10 -0300

    frontend component for home page

---
+++ Web/src/app/components/home/recommendation-box/recommendation-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recommendation-box',
+  templateUrl: './recommendation-box.component.html',
+  styleUrls: ['./recommendation-box.component.css']
+})
+export class RecommendationBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
