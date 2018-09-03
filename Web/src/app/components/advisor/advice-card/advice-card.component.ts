085ac419e4fa6048ac54376320c5c8d27b9e3a50
danielduartef <danielduartefigueiredo@gmail.com>
2018-09-03 15:28:12 -0300

    recommendation box

---
+++ Web/src/app/components/advisor/advice-card/advice-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'advice-card',
+  templateUrl: './advice-card.component.html',
+  styleUrls: ['./advice-card.component.css']
+})
+export class AdviceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Web/src/app/components/home/recommendation-box/recommendation-box.component.ts
+++ Web/src/app/components/home/recommendation-box/recommendation-box.component.ts
@@ -9,6 +9,9 @@ import { AdvisorService } from '../../../services/advisor.service';
 })
 export class RecommendationBoxComponent implements OnInit {
   advices : FeedResponse[] = [];
+  buyAdvices : FeedResponse[] = [];
+  sellAdvices : FeedResponse[] = [];
+  closePositionAdvices : FeedResponse[] = [];
 
   constructor(private advisorServices:AdvisorService) { }
 
@@ -19,6 +22,13 @@ export class RecommendationBoxComponent implements OnInit {
   appendRecommendations(){
     this.advisorServices.listLatestAdvicesForEachType(3).subscribe(result => 
       this.advices = this.advices.concat(result));
+      this.sellAdvices = this.advices.filter(advice => advice.adviceType == 0);
+      this.buyAdvices = this.advices.filter(advice => advice.adviceType == 1);
+      this.closePositionAdvices = this.advices.filter(advice => advice.adviceType == 2);
+  }
+
+  accessRecommendations(){
+    alert('test');
   }
 
 }
