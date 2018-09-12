5d4f357a4e426fda48d2475dee9ca50ebb5ab6b7
thiagomva <thiagomvaraujo@gmail.com>
2018-09-12 13:22:14 -0300

    Details

--- Web/src/app/components/advisor/expert-details/expert-details.component.ts
+++ Web/src/app/components/advisor/expert-details/expert-details.component.ts
@@ -15,9 +15,9 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./expert-details.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
-      state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*', display: 'table-row' })),
+      transition('expanded <=> collapsed', animate('5ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
