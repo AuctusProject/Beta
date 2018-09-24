565991c2faceeea538299db8e774ce766718c17c
arinyguedes <arinyguedes@gmail.com>
2018-09-24 14:00:10 -0300

    Text adjustments

--- Web/src/app/components/advisor/advices/advices.component.ts
+++ Web/src/app/components/advisor/advices/advices.component.ts
@@ -67,6 +67,6 @@ export class AdvicesComponent implements OnInit {
   }
 
   getTopText() : string {
-    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    return "The most popular Cryptocurrencies. Follow your favorite coins and see what analysts are saying about them";
   }
 }
