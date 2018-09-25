0e696b905a2cbd3b2fc991592a1ebc57409db32c
danielduartef <danielduartefigueiredo@gmail.com>
2018-09-25 19:00:35 -0300

    including classes for early acess subscription

---
+++ DataAccess/Account/EarlyAccessEmailData.cs
@@ -0,0 +1,19 @@
+﻿using Auctus.DataAccess.Core;
+using Auctus.DataAccessInterfaces.Account;
+using Auctus.DomainObjects.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Auctus.DataAccess.Account
+{
+    public class EarlyAccessEmailData : BaseSql<EarlyAccessEmail>, IEarlyAccessEmailData<EarlyAccessEmail>
+    {
+        public override string TableName => "EarlyAccessEmail";
+
+        public EarlyAccessEmailData(IConfigurationRoot configuration) : base(configuration)
+        {
+        }
+    }
+}
