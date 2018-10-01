de64efa30bfa8c4e7ac33d4fa562c94bf4903ecc
arinyguedes <arinyguedes@gmail.com>
2018-10-01 12:20:43 -0300

    Dashboard adjustment

--- Business/Account/ActionBusiness.cs
+++ Business/Account/ActionBusiness.cs
@@ -94,14 +94,14 @@ namespace Auctus.Business.Account
             var activities = Task.Factory.StartNew(() => Data.FilterActivity(cutDayForActivity, ActionType.NewAucVerification, ActionType.NewLogin, ActionType.NewWallet));
             Task.WaitAll(users, requestsToBeAdvisor, advices, advisorFollowers, assetFollowers, activities);
 
-            var adminsId = users.Result.Where(c => Admins?.Any(a => a == c.Email) == true).Select(c => c.Id);
-            var consideredUsers = users.Result.Where(c => !adminsId.Contains(c.Id));
-            var consideredAdvisors = advisors.Where(c => !adminsId.Contains(c.Id));
-            var consideredRequestsToBeAdvisor = requestsToBeAdvisor.Result.Where(c => !adminsId.Contains(c.UserId));
-            var consideredAdvices = advices.Result.Where(c => !adminsId.Contains(c.AdvisorId));
-            var consideredAdvisorFollowers = advisorFollowers.Result.Where(c => !adminsId.Contains(c.UserId));
-            var consideredAssetFollowers = assetFollowers.Result.Where(c => !adminsId.Contains(c.UserId));
-            var consideredActivities = activities.Result.Where(c => !adminsId.Contains(c.UserId));
+            var adminsId = users.Result.Where(c => Admins?.Any(a => a == c.Email) == true).Select(c => c.Id).ToHashSet();
+            var consideredUsers = users.Result.Where(c => !adminsId.Contains(c.Id) && (!c.ReferredId.HasValue || !adminsId.Contains(c.ReferredId.Value))).ToList();
+            var consideredAdvisors = advisors.Where(c => !adminsId.Contains(c.Id)).ToList();
+            var consideredRequestsToBeAdvisor = requestsToBeAdvisor.Result.Where(c => !adminsId.Contains(c.UserId)).ToList();
+            var consideredAdvices = advices.Result.Where(c => !adminsId.Contains(c.AdvisorId)).ToList();
+            var consideredAdvisorFollowers = advisorFollowers.Result.Where(c => !adminsId.Contains(c.UserId)).ToList();
+            var consideredAssetFollowers = assetFollowers.Result.Where(c => !adminsId.Contains(c.UserId)).ToList();
+            var consideredActivities = activities.Result.Where(c => !adminsId.Contains(c.UserId)).ToList();
 
             var result = new DashboardResponse();
             result.TotalUsersConfirmed = consideredUsers.Count(c => c.Wallets.Any() && !consideredAdvisors.Any(a => a.Id == c.Id));
