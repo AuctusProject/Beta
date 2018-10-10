2da18a87b3ae8c425e953b984fd6a933105ebd45
thiagomva <thiagomvaraujo@gmail.com>
2018-10-10 12:05:34 -0300

    adjusments

--- Web/src/app/components/account/feed/event-card/event-card.component.ts
+++ Web/src/app/components/account/feed/event-card/event-card.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FeedResponse } from '../../../../model/advisor/feedResponse';
 import { CONFIG } from '../../../../services/config.service';
 import { NavigationService } from '../../../../services/navigation.service';
+import { AccountService } from '../../../../services/account.service';
+import { AssetService } from '../../../../services/asset.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'event-card',
@@ -10,7 +13,11 @@ import { NavigationService } from '../../../../services/navigation.service';
 })
 export class EventCardComponent implements OnInit {
   @Input() eventFeed : FeedResponse;
-  constructor(private navigationService : NavigationService) { }
+  promise : Subscription;
+  
+  constructor(private assetService : AssetService,
+    private navigationService: NavigationService,
+    private accountService: AccountService) { }
 
   ngOnInit() {
   }
@@ -30,5 +37,19 @@ export class EventCardComponent implements OnInit {
 
   goToAssetDetails(){
     this.navigationService.goToAssetDetails(this.eventFeed.assetId);
+  }
+
+  onFollowClick(event: Event){
+    if(this.accountService.hasInvestmentToCallLoggedAction()){
+      this.promise = this.assetService.followAsset(this.eventFeed.assetId).subscribe(result =>
+          this.eventFeed.followingAsset = true
+      );
+    }
+    event.stopPropagation();
+  }
+  
+  onUnfollowClick(event: Event){
+    this.promise = this.assetService.unfollowAsset(this.eventFeed.assetId).subscribe(result =>this.eventFeed.followingAsset = false);
+    event.stopPropagation();
   }
 }
