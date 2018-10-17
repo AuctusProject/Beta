aadf901fcd389fe0f97092d1839cfcbf2cdad8b4
thiagomva <thiagomvaraujo@gmail.com>
2018-10-17 17:35:16 -0300

    Layout adjusments

--- Web/src/app/components/terminal/terminal.component.ts
+++ Web/src/app/components/terminal/terminal.component.ts
@@ -17,11 +17,25 @@ export class TerminalComponent implements OnInit {
   constructor(private assetService: AssetService) { }
 
   ngOnInit() {
+    this.loadScript();
     this.assetService.getTerminalAssets().subscribe(result =>
       {
         this.assets = result;
         this.setSelectedAsset(this.assets[0]);
       });
+  }
+
+  loadScript(){
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.text = "window._urq = window._urq || []; "+
+    " _urq.push(['initSite', 'd017ccc3-e423-46a6-ac7e-111f43466da6']); "+
+    " (function() { " +
+    " var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true; " +
+    " ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js'); " +
+    " var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s); " +
+    " })();";
+    document.getElementsByTagName("body")[0].appendChild(script);
   }
 
   getLogoImgUrl() {
