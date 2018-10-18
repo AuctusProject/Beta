0e1f4566f63ef321c1e43c6bf22732f844eb03e5
thiagomva <thiagomvaraujo@gmail.com>
2018-10-18 14:21:05 -0300

    New rating button

--- Web/src/app/components/terminal/ratings-list/ratings-list.component.ts
+++ Web/src/app/components/terminal/ratings-list/ratings-list.component.ts
@@ -5,6 +5,8 @@ import { AssetRatingsResponse } from 'src/app/model/asset/assetRatingsResponse';
 import { Util } from 'src/app/util/Util';
 import { ValueDisplayPipe } from 'src/app/util/value-display.pipe';
 import { NavigationService } from 'src/app/services/navigation.service';
+import { AccountService } from 'src/app/services/account.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'ratings-list',
@@ -15,11 +17,16 @@ export class RatingsListComponent implements OnInit, OnChanges {
   @Input() assetId: number;
   displayedColumns: string[] = ['time','recommendation','target','stopLoss','expert','rating'];
   ratings: AssetRatingsResponse[];
+  showRatingButton: boolean;
   constructor(private assetService: AssetService,
-    private navigationService: NavigationService) { }
+    private navigationService: NavigationService,
+    private accountService: AccountService,
+    private modalService: ModalService) { }
 
   ngOnInit() {
-  }
+    let loginData = this.accountService.getLoginData();
+    this.showRatingButton = !!loginData && loginData.isAdvisor;
+  }  
 
   ngOnChanges() {
     this.ratings = null;
@@ -46,5 +53,9 @@ export class RatingsListComponent implements OnInit, OnChanges {
   
   getAdviceTypeColor(type: number){
     return Util.GetRecommendationTypeColor(type);
+  }
+
+  onNewAdviceClick() {
+    this.modalService.setNewAdvice(this.assetId);
   }
 }
