119e913b5e04c9fd6c46fe6fdd75723152dafc89
arinyguedes <arinyguedes@gmail.com>
2018-11-05 14:39:03 -0200

    bug fix

--- Web/src/app/components/util/percentage-display/percentage-display.component.ts
+++ Web/src/app/components/util/percentage-display/percentage-display.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'percentage-display',
   templateUrl: './percentage-display.component.html',
   styleUrls: ['./percentage-display.component.css']
 })
-export class PercentageDisplayComponent implements OnInit {
+export class PercentageDisplayComponent implements OnChanges {
   @Input() value: number;
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
     this.value = Math.round(this.value * 10000) / 100;
   }
 
